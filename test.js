bfce6aa72c27b5ad32ff1946142dec46fddf4761
Reid Burke <me@reidburke.com>
2011-02-24 09:28:04 -0800

    Add 302 test with relative path.

--- test.js
+++ test.js
@@ -29,7 +29,35 @@ vows.describe("Pact").addBatch({
             topic: pact.request(),
             "should succeed" : function (topic) {
                 assert.strictEqual(topic.status, 200);
+            },
+            "contains correct header" : function (topic) {
+                assert.include(topic.headers, "content-type");
+                assert.strictEqual(topic.headers["content-type"], headers["Content-Type"]);
+            },
+            "contains valid data" : function (topic) {
                 assert.strictEqual(topic.body, "ok!");
+            }
+        }
+    },
+    "A 302 redirecting server with a relative path" : {
+        topic : mockServer(function (req, res) {
+            if (req.url === "/ok") {
+                res.writeHead(200, headers);
+                res.end("tango");
+            } else {
+                res.writeHead(302, {
+                    "Location" : "/ok"
+                });
+                res.end();
+            }
+        }),
+        "when / is requested" : {
+            topic : pact.request(),
+            "should be 200 instead of 302" : function (topic) {
+                assert.strictEqual(topic.status, 200);
+            },
+            "contains valid data" : function (topic) {
+                assert.strictEqual(topic.body, "tango");
             }
         }
     }
